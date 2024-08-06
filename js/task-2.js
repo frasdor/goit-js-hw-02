c1d1668ebca018d221bf41fb5a68b7183c29dd19
frasdor <frasunkiewicz.dorota@gmail.com>
2024-08-06 10:29:46 +0200

    added index.html

--- js/task-2.js
+++ js/task-2.js
@@ -9,8 +9,6 @@ function formatMessage(message, maxLength){
 }
 
 
-
-
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
 console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
